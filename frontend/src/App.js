76551e215b24f4d8b1bd48cf59cbedda71becd07
idkwhattosay <92638932+idkwhttsay@users.noreply.github.com>
2024-04-07 19:40:38 +0500

    feat: news categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,42 @@ function App() {
     }
   }
 
+  const getLife = async() => {
+    try {
+      const response = await axios.get("http://localhost:3001/life");
+      setArticles(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getSport = async() => {
+    try {
+      const response = await axios.get("http://localhost:3001/sport");
+      setArticles(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getEdu = async() => {
+    try {
+      const response = await axios.get("http://localhost:3001/edu");
+      setArticles(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getTravel = async() => {
+    try {
+      const response = await axios.get("http://localhost:3001/travel");
+      setArticles(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const seacrhArticles = async() => {
     try {
       const response = await axios.get(`http://localhost:3001/search/${searchName}`);
@@ -35,12 +71,29 @@ function App() {
     <>
       <div className="header">
         <h1>Новости Казахстана</h1>
-          <button class="button-4" style={{marginLeft: "50%"}} onClick={() => {
-            seacrhArticles();
-          }}>Поиск</button>
+          <div class="dropdown" style={{marginLeft: "40%"}}>
+            <button class="button-4">Категории Новостей</button>
+            <div class="dropdown-content">
+              <button href="#" onClick={() => {
+                getLife();
+              }}>Жизнь</button>
+              <button href="#" onClick={() => {
+                getSport();
+              }}>Спорт</button>
+              <button href="#" onClick={() => {
+                getTravel();
+              }}>Путешествия</button>
+              <button href="#" onClick={() => {
+                getEdu();
+              }}>Образование</button>
+            </div>
+          </div>
           <button class="button-4" onClick={() => {
             getArticles();
           }}>Все Новости</button>
+          <button class="button-4" onClick={() => {
+            seacrhArticles();
+          }}>Поиск</button>
           <div class="form__group field">
             <input class="form__field" placeholder="Поиск" name="Новости" id='name' value={searchName} onChange={e => setSearchName(e.target.value)} />
             <label for="name" class="form__label">Поиск</label>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import RSSParser from "rss-parser";
 
 const feedURL = "https://rss.app/feeds/JtWqZ9j1MGcmIdig.xml";
+const sportURL = "https://rss.app/feeds/Xr9CLD1w2JQTJxg7.xml";
+const lifeURL = "https://rss.app/feeds/f2NttkyvGg6jsY3F.xml";
+const eduURL = "https://rss.app/feeds/SZFLk4lqej5hYDyY.xml";
+const travelURL = "https://rss.app/feeds/i2FPy18Z1lpmzevt.xml";
 const parser = new RSSParser();
 
 let app = express();
@@ -12,8 +16,8 @@ const server = app.listen("3001", () => {
     console.log("App is listening at http://localhost:3001");
 });
 
-const parse = async () => {
-    const feed = await parser.parseURL(feedURL);
+const parse = async url => {
+    const feed = await parser.parseURL(url);
 
     let articles = [];
 
@@ -49,14 +53,32 @@ const search = async (str) => {
 
 app.get('/all', async (request, response) => {
     console.log(`GET on /all`);
-    // console.log(await parse(feedURL));
     response.send( await parse(feedURL));
+});
+
+app.get('/edu', async (request, response) => {
+    console.log(`GET on /edu`);
+    response.send( await parse(eduURL));
+});
+
+app.get('/sport', async (request, response) => {
+    console.log(`GET on /sport`);
+    response.send( await parse(sportURL));
+});
+
+app.get('/travel', async (request, response) => {
+    console.log(`GET on /travel`);
+    response.send( await parse(travelURL));
+});
+
+app.get('/life', async (request, response) => {
+    console.log(`GET on /life`);
+    response.send( await parse(lifeURL));
 });
 
 app.get('/search/:str', async (request, response) => {
     const str = request.params.str;
     console.log(`GET on /search/:str: ${str}`);
-    // console.log(await search(str));
     response.send(await search(str));
 });
 
